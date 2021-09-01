9a416563d3aa1b539d427716f145d06ad26c6245
Martin Lohrer <promalo@gmail.com>
2021-09-01 16:10:25 +0100

    Created staging branch

--- components/cover-image.js
+++ components/cover-image.js
@@ -9,7 +9,7 @@ export default function CoverImage({ title, coverImage, slug }) {
       height={1000}
       alt={`Cover Image for ${title}`}
       src={coverImage?.sourceUrl}
-      className={cn('shadow-small', {
+      className={cn('shadow-small object-cover', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
     />
