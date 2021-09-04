23df8099d6e5d8e66a9a177603c9564d21308447
Martin Lohrer <promalo@gmail.com>
2021-09-04 21:23:24 +0100

    pinkeries

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Navbar() {
     return (
-        <nav className="flex-col sm:flex-row flex items-center md:justify-between justify-center min-w-full bg-uil-grey">
+        <nav className="flex-col sm:flex-row flex items-center md:justify-between justify-center min-w-full bg-[#FFF5F2]">
             <div className="container mx-auto px-5 py-4 md:flex md:flex-row space-y-2 md:space-y-0 whitespace-nowrap md:align-middle">
                 <a href="/" className="xl:absolute font-adriane-text-bold text-2xl text-white bg-uil-key px-2 mx-2">London Ukrainian Review</a>
                 <ul className="flex-col sm:flex-row sm:align-middle sm:content-center flex space-y-2 sm:space-y-0 sm:space-x-4 lg:space-x-4 justify-center mx-auto text-lg text-uil-key px-2">
