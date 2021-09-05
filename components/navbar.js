fc6b5aad0c2324722b3e26e389e51c07dffedf70
Martin Lohrer <promalo@gmail.com>
2021-09-05 15:41:28 +0100

    Added links to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -8,9 +8,9 @@ export default function Navbar() {
                 <a href="/" className="xl:absolute font-adriane-text-bold text-2xl text-white bg-uil-key px-2 mx-2">London Ukrainian Review</a>
                 <ul className="flex-col sm:flex-row sm:align-middle sm:content-center flex space-y-2 sm:space-y-0 sm:space-x-4 lg:space-x-4 justify-center mx-auto text-lg text-uil-key px-2">
                     <a href="/"><li className=" text-uil-key current-menu-item">Current Issue</li></a>
-                    <li>About</li>
-                    <li>Archive</li>
-                    <li>Support Us</li>
+                    <a href="/posts/about-us"><li>About</li></a>
+                    <a href="/posts/archive-post"><li>Archive</li></a>
+                    <a href="/posts/support-us-post"><li>Support Us</li></a>
                 </ul>
                 <a href="https://ukrainianinstitute.org.uk/" target="_blank" className="flex flex-col w-18 h-12 xl:absolute sm:right-12 sm:bottom-2 sm:top-2 font-adriane-text-bold text-2xl text-white px-2"><img className="w-full h-full p-1" alt="Ukrainian Institute London Logo" src="/images/uil-logo.svg" /> </a>
             </div>
