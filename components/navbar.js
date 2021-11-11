a7624087103a41105c06fec5bf3ce914d8487926
Martin Lohrer <promalo@gmail.com>
2021-11-11 15:28:10 +0100

    fixed navbar overlap on small mobile screens

--- components/navbar.js
+++ components/navbar.js
@@ -8,9 +8,9 @@ export default function Navbar( {slug} ) {
             <div className='container mx-auto px-5 sm:pl-7 pt-4 sm:pt-3 pb-2'>
                 <span className='font-adriane-text-bold text-white px-2 bg-uil-key text-base tracking-widest'>Special Publication of the Ukrainian Institute London</span>
             </div>
-            <div className="container mx-auto px-5 sm:pl-7 pt-0 pb-2 md:flex md:flex-row space-y-2 md:space-y-0 whitespace-nowrap md:align-middle items-center">
+            <div className="container mx-auto px-5 sm:pl-7 pt-0 pb-2 md:flex md:flex-row space-y-2 md:space-y-0 sm:whitespace-nowrap md:align-middle items-center">
                 <div className="flex-1 flex justify-start">
-                    <a href="/" className="font-adriane-text-bold text-2xl text-white bg-uil-key px-2">London Ukrainian Review</a>
+                    <a href="/" className="font-adriane-text-bold text-2xl sm:text-2xl py-1 sm:py-0 text-white bg-uil-key px-2">London Ukrainian Review</a>
                 </div>
                 <ul className="flex-initial flex-col sm:flex-row sm:align-middle sm:content-center flex space-y-2 sm:space-y-0 sm:space-x-4 lg:space-x-4 justify-end mx-auto text-lg text-uil-key px-14">
                     <li className={cn({
