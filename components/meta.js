3f13297d40b496dc35378c4f4fe44f9d4617decd
Martin Lohrer <promalo@gmail.com>
2021-11-28 17:31:02 +0100

    Adjusted theme-color

--- components/meta.js
+++ components/meta.js
@@ -30,7 +30,7 @@ export default function Meta() {
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#FFE2D8" />
+      <meta name="theme-color" content="#FFF5F2" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
