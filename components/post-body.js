5fd9f576529739f2340871c17e218e4009334c49
Martin Lohrer <promalo@gmail.com>
2021-11-30 18:32:14 +0100

    Fixed break-words attributes

--- components/post-body.js
+++ components/post-body.js
@@ -4,7 +4,8 @@ export default function PostBody({ content }) {
   return (
     <div className="max-w-3xl mx-auto px-2">
       <div
-        className={styles.content}
+        // className={styles.content}
+        className={`${styles.content} max-w-full break-words`}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
