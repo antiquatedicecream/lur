6f5b401599eabca383ec63d3a724aeeb2fa894a1
Martin Lohrer <promalo@gmail.com>
2021-12-05 14:48:34 +0000

    Added h1 header

--- components/navbar.js
+++ components/navbar.js
@@ -10,7 +10,7 @@ export default function Navbar( {slug} ) {
             </div>
             <div className="container mx-auto px-5 sm:pl-7 pt-0 pb-2 md:flex md:flex-row space-y-2 md:space-y-0 sm:whitespace-nowrap md:align-middle items-center">
                 <div className="flex-1 flex justify-start">
-                    <a href="/" className="font-adriane-text-bold text-2xl sm:text-2xl py-1 sm:py-0 text-white bg-uil-key px-2">London Ukrainian Review</a>
+                    <a href="/" className="font-adriane-text-bold text-2xl sm:text-2xl py-1 sm:py-0 text-white bg-uil-key px-2"><h1>London Ukrainian Review</h1></a>
                 </div>
                 <ul className="flex-initial flex-col sm:flex-row sm:align-middle sm:content-center flex space-y-2 sm:space-y-0 sm:space-x-4 lg:space-x-4 justify-end mx-auto text-lg text-uil-key px-14">
                     <li className={cn({
