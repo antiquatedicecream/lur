57aafd7ec4aaff8205e3d58768b25b8bd260746d
Martin Lohrer <promalo@gmail.com>
2023-08-13 18:31:58 +0200

    reprint-preview.js refactoring

--- components/reprint-preview.js
+++ components/reprint-preview.js
@@ -1,50 +1,43 @@
-import Avatar from '../components/avatar'
-import Date from '../components/date'
-import CoverImage from './cover-image'
-import Link from 'next/link'
-import {categoriesContain, REPRINT_MARKER} from "../lib/categories";
+import Avatar from '../components/avatar';
+import Date from '../components/date';
+import CoverImage from './cover-image';
+import Link from 'next/link';
 
 export default function ReprintPreview({
-    title,
-    coverImage,
-    date,
-    excerpt,
-    author,
-    slug,
-    categories,
-}) {
+                                         title,
+                                         coverImage,
+                                         date,
+                                         excerpt,
+                                         author,
+                                         slug,
+                                         categories,
+                                       }) {
 
-    const isReprint = categoriesContain(categories, REPRINT_MARKER);
-
-    return (
-        <>
-            {isReprint ? (
-                <div className="bg-uil-post">
-                    <div className="mb-2 xl:mg-5">
-                        {coverImage && (
-                            <CoverImage title={title} coverImage={coverImage} slug={slug} />
-                        )}
-                    </div>
-                    <div className="mb-3 mx-3">
-                        <h3 className="text-4xl font-adriane-text-bold mb-0 leading-snug text-uil-key">
-                            <Link href={`/posts/${slug}`}>
-                                <a
-                                    className="hover:underline"
-                                    dangerouslySetInnerHTML={{ __html: title }}
-                                ></a>
-                            </Link>
-                        </h3>
-                        <div className="xl:text-lg mb-4 font-adriane-text-italic">
-                            <Date dateString={date} />
-                        </div>
-                        <div
-                            className="text-lg leading-relaxed mb-4"
-                            dangerouslySetInnerHTML={{ __html: excerpt }}
-                        />
-                        <Avatar author={author} />
-                    </div>
+  return (<>
+        {(<div className="bg-uil-post">
+              <div className="mb-2 xl:mg-5">
+                {coverImage &&
+                    (<CoverImage title={title} coverImage={coverImage}
+                                 slug={slug}/>)}
+              </div>
+              <div className="mb-3 mx-3">
+                <h3 className="text-4xl font-adriane-text-bold mb-0 leading-snug text-uil-key">
+                  <Link href={`/posts/${slug}`}>
+                    <a
+                        className="hover:underline"
+                        dangerouslySetInnerHTML={{__html: title}}
+                    ></a>
+                  </Link>
+                </h3>
+                <div className="xl:text-lg mb-4 font-adriane-text-italic">
+                  <Date dateString={date}/>
                 </div>
-            ) : ('')}
-        </>
-    )
+                <div
+                    className="text-lg leading-relaxed mb-4"
+                    dangerouslySetInnerHTML={{__html: excerpt}}
+                />
+                <Avatar author={author}/>
+              </div>
+            </div>)}
+      </>);
 }
