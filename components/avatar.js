40c3f56d9b28902e0f7e408d559f5497746d1d28
Martin Lohrer <promalo@gmail.com>
2023-12-28 20:53:53 +0100

    added translator links

--- components/avatar.js
+++ components/avatar.js
@@ -1,6 +1,3 @@
-import Image from 'next/image'
-import Link from 'next/link';
-
 export default function Avatar({ author, route }) {
   const name = author
     ? author.firstName && author.lastName
