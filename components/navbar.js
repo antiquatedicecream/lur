12ebd95d8f882930ddd29620d87d335e80ea7d65
Martin Lohrer <promalo@gmail.com>
2024-02-11 13:21:46 +0100

    - updated menu

--- components/navbar.js
+++ components/navbar.js
@@ -5,12 +5,20 @@ import cn from 'classnames'
 export default function Navbar( {slug, route} ) {
     return (
         <nav className="min-w-full bg-uil-post">
-            <div className="container mx-auto px-5 sm:pl-7 pt-4 sm:pt-3 pb-2 md:flex md:flex-row space-y-2 md:space-y-0 sm:whitespace-nowrap md:align-middle items-center">
+            <div
+              className="container flex flex-col sm:flex-row mx-auto md:px-5 sm:pl-7 pt-4 sm:pt-3 pb-2 md:flex md:flex-row space-y-2 md:space-y-0 md:whitespace-nowrap md:align-middle items-center">
                 <div className="flex-1 flex justify-start">
-                    <a href="/" className="font-adriane-text-bold text-2xl sm:text-2xl py-1 sm:py-0 text-white bg-uil-key px-2"><h1>London Ukrainian Review</h1></a>
+                    <div
+                      className={'font-adriane-text-bold h-full text-8xl md:text-9xl py-1 sm:py-0 text-uil-key pr-3 sm:px-3'}>LUR
+                    </div>
+                    <a href="/"
+                       className="font-adriane-text-bold text-2xl sm:text-2xl py-1 sm:py-0 text-white bg-uil-key px-3 flex flex-col justify-center">
+                        <h1>London</h1>
+                        <h1>Ukrainian</h1>
+                        <h1>Review</h1></a>
                 </div>
                 <ul
-                  className="flex-initial flex-col sm:flex-row sm:align-middle sm:content-center flex space-y-2 sm:space-y-0 sm:space-x-4 lg:space-x-4 justify-end mx-auto text-lg text-uil-key">
+                  className="grid grid-cols-2 gap-2 text-center sm:flex-initial sm:flex-row sm:align-middle sm:content-center sm:flex  sm:space-y-0 sm:space-x-4 lg:space-x-4 justify-end mx-auto text-lg text-uil-key">
                     <li className={cn({
                         'current-menu-item': slug !== 'about-us'
                           && slug !== 'archive-post'
