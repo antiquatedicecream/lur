a7373ba2377b490e03a3d8ab1f4346fe44cadfad
Martin Lohrer <promalo@gmail.com>
2024-02-27 12:46:58 +0100

    Revert "change to golden ratio"
    This reverts commit 92ff526ad2db98362a204e9abd9c55c75b3e4cdd.

--- components/cover-image.js
+++ components/cover-image.js
@@ -6,7 +6,7 @@ export default function CoverImage({ title, coverImage, slug, route }) {
   const routeString = route ? route : 'posts';
   const image = (
     <Image
-      width={1600}
+      width={2000}
       height={1000}
       alt={coverImage ? `Cover Image for ${title}` : ' '}
       src={coverImage?.sourceUrl}
