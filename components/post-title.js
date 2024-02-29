279948c2dd40a09548e01ffa0e30a7e7058f3189
Martin Lohrer <promalo@gmail.com>
2024-02-29 21:43:12 +0100

    Archive dropdown

--- components/post-title.js
+++ components/post-title.js
@@ -1,7 +1,7 @@
 export default function PostTitle({ children }) {
   return (
     <h1
-      className="text-4xl 2xl:text-6xl  font-bold tracking-tighter leading-tight md:leading-none text-center md:text-left"
+      className="text-4xl 2xl:text-6xl  font-bold tracking-tighter leading-tight md:leading-none text-left"
       dangerouslySetInnerHTML={{ __html: children }}
     />
   )
